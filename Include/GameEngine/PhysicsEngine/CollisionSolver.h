b8894d1f2896982c887751b263f5a3d26889b4e7
Nikikust <lis291@yandex.ru>
2023-11-07 13:42:19 +0300

    Rename struct

--- Include/GameEngine/PhysicsEngine/CollisionSolver.h
+++ Include/GameEngine/PhysicsEngine/CollisionSolver.h
@@ -9,7 +9,7 @@
 
 namespace physics
 {
-    struct ContactsInfo
+    struct ContactPointssInfo
     {
         sf::Vector2f collision_point_1;   // 1st for: circ - circ, circ - poly
         sf::Vector2f collision_point_2;   // 2nd for: poly - poly
