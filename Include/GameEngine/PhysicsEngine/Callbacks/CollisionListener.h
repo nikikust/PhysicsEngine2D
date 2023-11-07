d99e2a6b24358fe46146e3e1da0c6f91d843fd45
Nikikust <lis291@yandex.ru>
2023-11-07 15:15:02 +0300

    Create CollisionListener class

---
+++ Include/GameEngine/PhysicsEngine/Callbacks/CollisionListener.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <GameEngine/PhysicsEngine/CollisionContact.h>
+
+
+namespace physics
+{
+    /// @brief You must NOT create or destroy bodies, fixtures, shapes and so on in these callbacks!
+    class CollisionListener
+    {
+        virtual ~CollisionListener() = default;
+
+        virtual void begin_contact(CollisionContact* contact) {};
+        virtual void end_contact(CollisionContact* contact) {};
+    };
+} // namespace physics
